c1bc20019fdc942392445ee501e9439e573a086d
python-developer-algebra-klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-01-29 21:14:07 +0100

    Update m2_01_variables.py

--- m2_01_variables.py
+++ m2_01_variables.py
@@ -1,1 +1,11 @@
 print('Hello World!')
+
+
+# Ime, prezime, godinu rođenja, državu rođenja, status radnog odnosa, težinu te spol
+first_name = 'Pero'
+last_name = 'Peric'
+year_of_birth = 1998
+country_of_birth = "Hrvatska"
+is_employed = True
+weight = 75.75
+gender = 'M'
