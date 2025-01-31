e7adcca322a91fe744be117ec043442650cdce8b
python-developer-algebra-klujic <krunoslav.lujic@predavaci.algebra.hr>
2025-01-31 18:50:56 +0100

    Update m2_01_variables.py

--- m2_01_variables.py
+++ m2_01_variables.py
@@ -1,6 +1,6 @@
 print('Hello World!')
 
-
+# Kreirajte varijable (imenujte ih i dodijelite im odgovarajuću vrijednost) za:
 # Ime, prezime, godinu rođenja, državu rođenja, status radnog odnosa, težinu te spol
 first_name = 'Pero'
 last_name = 'Peric'
@@ -9,3 +9,14 @@ country_of_birth = "Hrvatska"
 is_employed = True
 weight = 75.75
 gender = 'M'
+
+
+
+# Stranice a i b, četverokuta te za površinu tog četverokuta.
+square_side_a = 5
+square_side_b = 8
+
+square_area = square_side_a * square_side_b
+
+print(square_area)
+print(country_of_birth)
